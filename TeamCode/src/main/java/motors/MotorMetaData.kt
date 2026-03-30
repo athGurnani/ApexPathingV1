eb550ce8385f73e8d07ec6ebf856578787392e15
athGurnani <atharvgurnani@gmail.com>
2026-03-30 18:56:32 -0500

    Convert MetaData to java

--- TeamCode/src/main/java/motors/MotorMetaData.kt
+++ TeamCode/src/main/java/motors/MotorMetaData.kt
@@ -1,33 +1,60 @@
-package motors
+package motors;
 
-import android.graphics.Path
-import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-class MotorMetaData {
-    var name : String = "motor"
-    var direction : DcMotorSimple.Direction = DcMotorSimple.Direction.FORWARD
-    var brakeMode : DcMotor.ZeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
-    var runMode : DcMotor.RunMode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+public class MotorMetaData {
+    private String name = "motor";
+    private DcMotorSimple.Direction direction = DcMotorSimple.Direction.FORWARD;
+    private DcMotor.ZeroPowerBehavior brakeMode = DcMotor.ZeroPowerBehavior.FLOAT;
+    DcMotor.RunMode runMode = DcMotor.RunMode.STOP_AND_RESET_ENCODER;
 
-    constructor(name : String, direction : DcMotorSimple.Direction, brakeMode : DcMotor.ZeroPowerBehavior, runMode: DcMotor.RunMode) {
+    public MotorMetaData (String name, DcMotorSimple.Direction direction, DcMotor.ZeroPowerBehavior brakeMode, DcMotor.RunMode runMode) {
         this.name = name;
-        this.direction = direction
-        this.brakeMode = brakeMode
-        this.runMode = runMode
+        this.direction = direction;
+        this.brakeMode = brakeMode;
+        this.runMode = runMode;
     }
 
-    constructor(name : String, direction : DcMotorSimple.Direction, brakeMode : DcMotor.ZeroPowerBehavior) {
-        MotorMetaData(name, direction, brakeMode, DcMotor.RunMode.RUN_WITHOUT_ENCODERS)
+    public MotorMetaData (String name, DcMotorSimple.Direction direction, DcMotor.ZeroPowerBehavior brakeMode) {
+        this(name, direction, brakeMode, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
-    constructor(name : String, direction : DcMotorSimple.Direction) {
-        MotorMetaData(name, direction, DcMotor.ZeroPowerBehavior.FLOAT)
+    public MotorMetaData(String name, DcMotorSimple.Direction direction) {
+        this(name, direction, DcMotor.ZeroPowerBehavior.FLOAT);
     }
-    constructor(name : String) {
-        MotorMetaData(name, DcMotorSimple.Direction.FORWARD)
+    public MotorMetaData(String name) {
+        this(name, DcMotorSimple.Direction.FORWARD);
     }
-    constructor() {
-        MotorMetaData("motor")
+    public MotorMetaData() {
+        this("motor");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DcMotorSimple.Direction getDirection() {
+        return direction;
+    }
+    public void setDirection(DcMotorSimple.Direction direction) {
+        this.direction = direction;
+    }
+
+    public DcMotor.ZeroPowerBehavior getBrakeMode() {
+        return brakeMode;
+    }
+    public void setBrakeMode(DcMotor.ZeroPowerBehavior mode) {
+        this.brakeMode = mode;
+    }
+
+    public DcMotor.RunMode getRunMode() {
+        return runMode;
+    }
+    public void setRunMode(DcMotor.RunMode mode) {
+        this.runMode = mode;
     }
 }
