4c2ac2c00aab7e3dfd9c3d60abbf3591c41f0ba5
athGurnani <atharvgurnani@gmail.com>
2026-04-14 13:33:57 -0500

    Add JavaDoc comments to CommandScheduler

--- TeamCode/src/main/java/commands/CommandScheduler.java
+++ TeamCode/src/main/java/commands/CommandScheduler.java
@@ -12,6 +12,9 @@ import java.util.function.Consumer;
 
 /**
  * @author Sohum Arora 22985 Paraducks
+ * @author Atharv G - 13085 Bionic Dutch
+ *
+ * Schedules and runs commands and command groups.
  */
 public final class CommandScheduler {
 
@@ -37,7 +40,7 @@ public final class CommandScheduler {
     private final Map<Command, Boolean> toSchedule = new LinkedHashMap<>();
     private final List<Command> toCancel = new ArrayList<>();
 
-    CommandScheduler() {}
+    public CommandScheduler() {}
 
     public void addButton(Runnable button) {
         buttons.add(button);
@@ -55,6 +58,11 @@ public final class CommandScheduler {
         }
     }
 
+    /**
+     * Adds a command to the scheduler to check for every loop.
+     * @param interruptible Dictates if the command can be safely interrupted
+     * @param command       Action that will run when run() is called
+     */
     private void schedule(boolean interruptible, Command command) {
         if (inRunLoop) {
             toSchedule.put(command, interruptible);
@@ -72,16 +80,28 @@ public final class CommandScheduler {
         initCommand(command, interruptible);
     }
 
+    /**
+     * Adds commands to the scheduler to check for every loop.
+     * @param interruptible Dictates if the command can be safely interrupted
+     * @param commands       Actions that will run when run() is called
+     */
     public void schedule(boolean interruptible, Command... commands) {
         for (Command command : commands) {
             schedule(interruptible, command);
         }
     }
 
+    /**
+     * Adds commands to the scheduler to check for every loop.
+     * @param commands       Actions that will run when run() is called
+     */
     public void schedule(Command... commands) {
         schedule(true, commands);
     }
 
+    /**
+     * Updates the scheduler, running all commands with a criteria that returns true.
+     */
     public void run() {
         if (disabled) return;
 
@@ -122,6 +142,10 @@ public final class CommandScheduler {
         toCancel.clear();
     }
 
+    /**
+     * Remove commands from schedule queue.
+     * @param commands  Commands to cancel
+     */
     public void cancel(Command... commands) {
         if (inRunLoop) {
             toCancel.addAll(Arrays.asList(commands));
@@ -139,25 +163,62 @@ public final class CommandScheduler {
         }
     }
 
+    /**
+     * Removes all commands from queue.
+     */
     public void cancelAll() {
         for (Command command : new ArrayList<>(scheduledCommands.keySet())) {
             cancel(command);
         }
     }
 
+    /**
+     * Checks if commands are queued to run.
+     * @param commands  Commands to check
+     * @return  A set of booleans indicating if the respective command is scheduled or not.
+     */
     public boolean isScheduled(Command... commands) {
         return scheduledCommands.keySet().containsAll(Arrays.asList(commands));
     }
 
+    /**
+     * Completely remove commands and other data from scheduler, regardless of if a command is running or not.
+     */
     public synchronized void reset() {
         instance = null;
     }
 
+    /**
+     * Pauses function of the scheduler run() method, retaining command data.
+     */
     public void disable() { disabled = true; }
+
+    /**
+     * Resumes function of the scheduler run() method.
+     */
     public void enable() { disabled = false; }
 
+    /**
+     * Adds an initialization instruction to a registered Command.
+     * @param action    The action to run when the command is initialized
+     */
     public void onCommandInitialize(Consumer<Command> action) { initActions.add(action); }
+
+    /**
+     * Adds an execution instruction to a registered Command.
+     * @param action    The action to run when the command is executed
+     */
     public void onCommandExecute(Consumer<Command> action) { executeActions.add(action); }
+
+    /**
+     * Adds an interruption instruction to a registered Command.
+     * @param action    The action to run when the command is interrupted
+     */
     public void onCommandInterrupt(Consumer<Command> action) { interruptActions.add(action); }
+
+    /**
+     * Adds a finish instruction to a registered Command.
+     * @param action    Action to run when the command is finished
+     */
     public void onCommandFinish(Consumer<Command> action) { finishActions.add(action); }
 }
