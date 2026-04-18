a198cf0108034d3a85db9dd90c25db5d444d31e2
Dr Pixel Cat <71606625+DrPixelCat@users.noreply.github.com>
2026-04-18 08:05:07 -0500

    Wheel Direction Debugger OpMode (#65)
    * Added gradle stuff and heading tuner
    
    * got rid of line
    
    * Added WheelDirectionDebugger.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelDirectionDebugger.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class WheelDirectionDebugger extends OpMode {
+    DcMotor fl, fr, bl, br;
+
+    @Override
+    public void init() {
+        fl = hardwareMap.get(DcMotor.class, Constants.driveConstants.flData.getName());
+        fr = hardwareMap.get(DcMotor.class, Constants.driveConstants.frData.getName());
+        bl = hardwareMap.get(DcMotor.class, Constants.driveConstants.blData.getName());
+        br = hardwareMap.get(DcMotor.class, Constants.driveConstants.brData.getName());
+    }
+
+    @Override
+    public void loop() {
+        runMotor(fl, gamepad1.a, "Front Left");
+        runMotor(fr, gamepad1.b, "Front Right");
+        runMotor(bl, gamepad1.x, "Back Left");
+        runMotor(br, gamepad1.y, "Back Right");
+    }
+
+    private void runMotor(DcMotor motor, boolean btn, String name) {
+        motor.setPower(btn ? 0.3 : 0);
+        if (btn) telemetry.addLine(name + " Running!");
+    }
+}
